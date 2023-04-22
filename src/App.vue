751188f0563334430e95fed6dcaf1f4fd9c2df89
Aki-MLB <86222636+Aki-MLB@users.noreply.github.com>
2023-04-22 19:26:09 +0900

    third

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,60 @@
 <template>
   <v-app>
-    <Header v-if="movieFlag===false" @changePage="changePage" />
-    <v-content>
-      <TopPage v-if="pageId === 0" @emit-change="changeMovie"/>
-      <Works v-if="pageId === 1"/>
-      <Flow v-if="pageId === 2"/>
-      <Company v-if="pageId === 3"/>
-      <Contact v-if="pageId === 4"/>
-    </v-content>
-    <FooterComponent class='mt-10' />
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
   </v-app>
 </template>
+
 <script>
-import Header from './components/Header.vue'
-import TopPage from './components/TopPage.vue'
-import Works from './components/Works.vue'
-import Flow from './components/Flow.vue'
-import Company from './components/Company.vue'
-import Contact from './components/Contact.vue'
-import FooterComponent from './components/FooterComponent.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    Header, FooterComponent, TopPage, Works, Flow, Company, Contact
+    HelloWorld,
   },
+
   data: () => ({
-    pageId: 0,
-    movieFlag: false
+    //
   }),
-  methods: {
-    changePage (val) {
-      this.pageId = val
-    },
-    changeMovie (val) {
-      this.movieFlag = val
-    }
-  }
-}
+};
 </script>
-<style>
-.v-toolbar__title {
-  overflow: visible !important;
-  margin-right: 50px !important;
-}
-
-.v-app-bar__nav-icon {
-  @media (min-width: 960px) {
-    display: none !important;
-  }
-}
-.v-tabs {
-  display: none;
-
-  @media (min-width: 960px) {
-    display: block !important;
-  }
-}
-</style>
